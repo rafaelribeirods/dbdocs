6ffa5726f62409101679eb580203471980251c6a
Rafael Ribeiro da Silva <rafaelribeiros1997@gmail.com>
2025-07-19 21:47:09 -0300

    Update javascript packages and initialize shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,10 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
 import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { Button } from './components/ui/button';
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
-
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
-
     return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+            <Button>Click me</Button>
         </div>
     )
 }
